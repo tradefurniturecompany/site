d7cf5848dcf775ad83cd5f5d54cfd8816b86f87e
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-16 15:20:09 +0600

    https://github.com/tradefurniturecompany/site/issues/2

--- app/code/IWD/InfinityScroll/README.md
+++
@@ -1,7 +0,0 @@
-**IWD Infinity Scroll Extension**
-
-Magento version - 2.1.x
-
-Installation Guide - https://www.iwdagency.com/help/general-information/installing-iwd-extensions
-
-Support - extensions@iwdagency.com
--- app/code/IWD/InfinityScroll/view/frontend/requirejs-config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    map: {
-        "*": {
-            iwdInfinityScroll: 'IWD_InfinityScroll/js/infinity.scroll',
-            iwdInfinityScrollHelperUrl: 'IWD_InfinityScroll/js/helper/url'
-        }
-    }
-};
