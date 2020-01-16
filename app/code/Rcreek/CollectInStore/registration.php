62fe5a14bb5a5ccfe64f3c45ffa398cd90d57c87
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-16 16:59:58 +0600

    https://github.com/tradefurniturecompany/site/issues/8

--- app/code/Rcreek/CollectInStore/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Rcreek_CollectInStore',
-        __DIR__
-    );
