f4715a52ccf23335d80ccaeced1f78f157a6fc73
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-16 17:01:10 +0600

    https://github.com/tradefurniturecompany/site/issues/9

--- app/code/Rcreek/DimensionsPlusImporter/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Rcreek_DimensionsPlusImporter',
-        __DIR__
-    );
