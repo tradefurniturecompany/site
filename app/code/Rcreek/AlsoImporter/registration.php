ac32bb3fa9eee7ad1f182789f67f20dc09dab944
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-16 16:59:08 +0600

    https://github.com/tradefurniturecompany/site/issues/7

--- app/code/Rcreek/AlsoImporter/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Rcreek_AlsoImporter',
-        __DIR__
-    );
