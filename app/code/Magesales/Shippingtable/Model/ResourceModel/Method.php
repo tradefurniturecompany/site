25d081d904554a7cb3872f5bbbcfe442301b1459
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-16 16:12:40 +0600

    https://github.com/tradefurniturecompany/site/issues/4

--- app/code/Magesales/Shippingtable/Model/ResourceModel/Method.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Magesales\Shippingtable\Model\ResourceModel;
-class Method extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
-{
-    protected function _construct()
-    {    
-        $this->_init('shippingmethod', 'method_id');
-    }          
-}
--- app/code/Magesales/Shippingtable/registration.php
+++
@@ -1,2 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::MODULE,  'Magesales_Shippingtable',__DIR__);
