81660e124884291ff7868b7fecde8ec5a9032dbb
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-15 06:33:37 +0600

    https://github.com/tradefurniturecompany/site/issues/37

--- app/code/Customweb/RealexCw/Model/Cron/TransactionCleanup.php
+++ app/code/Customweb/RealexCw/Model/Cron/TransactionCleanup.php
@@ -62,7 +62,14 @@ class TransactionCleanup
 
 		// Remove all failed transactions after 2 months.
 		try {
-			$this->_logger->info('RealexCw: Cleaned up ' . $this->delete($this->getFailedTransactions(), $maxEndtime) . ' failed transactions.');
+			/**
+			 * 2020-02-15 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+			 * "Prevent Customweb_RealexCw from logging «RealexCw: Cleaned up 0 failed transactions»":
+			 * https://github.com/tradefurniturecompany/site/issues/37
+			 */
+			if ($c = $this->delete($this->getFailedTransactions(), $maxEndtime)) {/** @var int $c */
+				$this->_logger->info("RealexCw: Cleaned up $c failed transactions.");
+			}
 		} catch (\Exception $e) {
 			$this->_logger->error('Error in RealexCw transaction cleanup cron: ' . $e->getMessage());
 		}
