317ac21372926fa38688f523b08615cb590deeb7
Dmitry Fedyuk <dfediuk@gmail.com>
2020-02-18 11:57:14 +0600

    https://github.com/tradefurniturecompany/site/issues/55

--- app/etc/config.php
+++ app/etc/config.php
@@ -303,7 +303,7 @@ return [
         'SpringImport_CatalogDropdownSort' => 1,
         'TFC_Core' => 1,
         'TFC_Report' => 1,
-        'Temando_Shipping' => 1,
+        'Temando_Shipping' => 0,
         'Tigren_MultiCOD' => 1,
         'Trustedshops_Trustedshops' => 1,
         'Vertex_Tax' => 1,
