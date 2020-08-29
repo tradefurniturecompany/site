339da1349d7d07d20d4deaf789296c023b8cdcd4
Dmitry Fedyuk <dfediuk@gmail.com>
2020-08-29 13:13:01 +0600

    https://github.com/tradefurniturecompany/site/issues/19

--- app/code/Hotlink/Brightpearl/Model/Config/Source/Magento/Shipping/Method/Allowed.php
+++ app/code/Hotlink/Brightpearl/Model/Config/Source/Magento/Shipping/Method/Allowed.php
@@ -41,6 +41,11 @@ class Allowed extends \Magento\Shipping\Model\Config\Source\Allmethods
      */
     function toOptionArray($isActiveOnlyFlag=false)
     {
+		# 2020-08-29 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+		# «Array to string conversion
+		# in app/code/Hotlink/Brightpearl/Model/Config/Source/Magento/Shipping/Method/Allowed.php on line 66»:
+		# https://github.com/tradefurniturecompany/site/issues/19
+    	$isActiveOnlyFlag = true;
         $methods = array( array( 'value' => '', 'label' => ' ' ) );
         try
             {
