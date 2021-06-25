3a1627378116619074e461e7a443649d55896bf9
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-25 15:36:33 +0600

    "Refactor the Brightpearl module": https://github.com/tradefurniturecompany/site/issues/224

--- app/code/Hotlink/Framework/Model/Schedule/Cron/Config/Plugin.php
+++ app/code/Hotlink/Framework/Model/Schedule/Cron/Config/Plugin.php
@@ -4,16 +4,16 @@ namespace Hotlink\Framework\Model\Schedule\Cron\Config;
 class Plugin
 {
 
-    protected $handler;
+	protected $handler;
 
-    function __construct( \Hotlink\Framework\Model\Schedule\Cron\Inject $handler )
-    {
-        $this->handler = $handler;
-    }
+	function __construct( \Hotlink\Framework\Model\Schedule\Cron\Inject $handler )
+	{
+		$this->handler = $handler;
+	}
 
-    function afterGetJobs( $subject, $result )
-    {
-        return $this->handler->execute( $result );
-    }
+	function afterGetJobs( $subject, $result )
+	{
+		return $this->handler->execute( $result );
+	}
 
 }
