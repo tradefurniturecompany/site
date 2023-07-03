498feaa9fdaed052cd24af75e1fedd1f8334453a
Dmitrii Fediuk <dfediuk@gmail.com>
2023-07-03 06:27:36 +0300

    https://github.com/tradefurniturecompany/site/issues/259

--- vendor/magento/module-checkout/view/frontend/web/js/action/select-payment-method.js
+++ vendor/magento/module-checkout/view/frontend/web/js/action/select-payment-method.js
@@ -12,11 +12,16 @@ define([
     'use strict';
 
     return function (paymentMethod) {
-        if (paymentMethod) {
+		// 2023-07-03 Dmitrii Fediuk https://www.upwork.com/fl/mage2pro
+		// 1) A temporary quick and dirty workaround for the problem:
+		// «Property "DisableTmpl" does not have accessor method "getDisableTmpl"
+		// in class "Magento\Quote\Api\Data\PaymentInterface"»: https://github.com/tradefurniturecompany/site/issues/259
+		// 2) https://github.com/PayboxByVerifone/Magento-2.3.x/issues/4#issuecomment-598288973
+        /*if (paymentMethod) {
             paymentMethod.__disableTmpl = {
                 title: true
             };
-        }
+        }*/
         quote.paymentMethod(paymentMethod);
     };
 });
